d79e5368d903ffa289f434f8abb4f0ad04085066
Giri Purnama <hiroyuki.giri@gmail.com>
2022-07-04 19:04:45 +0700

    add assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ mix.js('resources/js/app.js', 'public/js')
+ .postCss('resources/css/template.css', 'public/assets/css', [
+     //
+ ])
+ .sass('resources/sass/core.scss', 'public/assets/css');
