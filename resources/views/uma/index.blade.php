73d06d8462df1dc964817fe392a8311ab18a10a8
Giri Purnama <hiroyuki.giri@gmail.com>
2022-07-20 20:12:54 +0700

    add table user

--- resources/views/uma/index.blade.php
+++ resources/views/uma/index.blade.php
@@ -10,16 +10,162 @@ User Management
 
 @section('content')
     
+<div class="wrapper">
+    <div class="content-user">
 
-Welcome UMA
+        <!-- Content Header (Page header) -->
+        <section class="content-header data-form">
+          <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
 
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">List User Management</h3>
+                            <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#create-user">
+                                Create User
+                            </button>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body p-0">
+                            <table id="user-table" class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 10px">#</th>
+                                        <th>Fullname</th>
+                                        <th>Username</th>
+                                        <th>Role</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1.</td>
+                                        <td>Giri Purnama</td>
+                                        <td>GiriPurnama</td>
+                                        <td>System Admin</td>
+                                        <td>
+                                            <span class="badge bg-success">Active</span>
+                                        </td>
+                                        <td>
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#edit-user"><span class="edit"><i class="ri-pencil-line"></i></span></a>
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#delete-user"><span class="delete"><i class="ri-delete-bin-line"></i></span></a>
+
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>2.</td>
+                                        <td>Giri Purnama</td>
+                                        <td>GiriPurnama</td>
+                                        <td>System Admin</td>
+                                        <td>
+                                            <span class="badge bg-success">Active</span>
+                                        </td>
+                                        <td>
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#edit-user"><span class="edit"><i class="ri-pencil-line"></i></span></a>
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#delete-user"><span class="delete"><i class="ri-delete-bin-line"></i></span></a>
+
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>3.</td>
+                                        <td>Giri Purnama</td>
+                                        <td>GiriPurnama</td>
+                                        <td>System Admin</td>
+                                        <td>
+                                            <span class="badge bg-success">Active</span>
+                                        </td>
+                                        <td>
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#edit-user"><span class="edit"><i class="ri-pencil-line"></i></span></a>
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#delete-user"><span class="delete"><i class="ri-delete-bin-line"></i></span></a>
+
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>4.</td>
+                                        <td>Giri Purnama</td>
+                                        <td>GiriPurnama</td>
+                                        <td>System Admin</td>
+                                        <td>
+                                            <span class="badge bg-success">Active</span>
+                                        </td>
+                                        <td>
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#edit-user"><span class="edit"><i class="ri-pencil-line"></i></span></a>
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#delete-user"><span class="delete"><i class="ri-delete-bin-line"></i></span></a>
+
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>5.</td>
+                                        <td>Giri Purnama</td>
+                                        <td>GiriPurnama</td>
+                                        <td>System Admin</td>
+                                        <td>
+                                            <span class="badge bg-success">Active</span>
+                                        </td>
+                                        <td>
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#edit-user"><span class="edit"><i class="ri-pencil-line"></i></span></a>
+                                            <a href="javascript:void(0);" data-toggle="modal" data-target="#delete-user"><span class="delete"><i class="ri-delete-bin-line"></i></span></a>
+
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+          </div><!-- /.container-fluid -->
+
+          <!-- Modal -->
+            <div class="modal fade" id="create-user">
+                <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <h4 class="modal-title">Large Modal</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    </div>
+                    <div class="modal-body">
+                    <p>One fine body&hellip;</p>
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+                <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
+          <!-- End Modal -->
+        </section>
+    
+    </div>
+</div>
 
 @stop
 
 @section('css')
-    
+<link href="{{ URL::asset('/assets/libs/datatables/datatables.min.css')}}" rel="stylesheet" type="text/css" />
 @stop
 
 @section('js')
-    
+<script src="{{ URL::asset('/assets/libs/datatables/datatables.min.js')}}"></script>
 @stop
+
+<script type="text/javascript">
+    $("#user-table").DataTable({
+      "responsive": true, 
+      "lengthChange": false, 
+      "autoWidth": false
+    });
+//     $(document).ready(function() {
+
+   
+//    });
+</script>
--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -30,11 +30,19 @@
         {{-- Configured Stylesheets --}}
         @include('adminlte::plugins', ['type' => 'css'])
         <link rel="stylesheet" href="{{ asset('assets/css/core.css') }}">
+        <link href="{{ URL::asset('assets/libs/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{ URL::asset('assets/libs/owl-carousel/assets/owl.carousel.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{ URL::asset('assets/libs/owl-carousel/assets/owl.theme.default.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{ URL::asset('assets/css/icons.css')}}" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
     @else
         <link rel="stylesheet" href="{{ mix(config('adminlte.laravel_mix_css_path', 'css/app.css')) }}">
     @endif
+
+    <!-- JS -->
+    <script src="{{ URL::asset('/assets/libs/jquery/jquery.min.js')}}"></script>
+    <script src="{{ URL::asset('/assets/libs/owl-carousel/owl.carousel.min.js')}}"></script>
 
     {{-- Livewire Styles --}}
     @if(config('adminlte.livewire'))
@@ -80,7 +88,6 @@
 
     {{-- Base Scripts --}}
     @if(!config('adminlte.enabled_laravel_mix'))
-        <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
         <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
         <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 
